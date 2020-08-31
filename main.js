dba2652a387b0b60dd650b274ffb4380b008b5a3
statesmans <mr.siroshtan@list.ru>
2020-08-31 19:08:30 +0300

    map navigation

--- main.js
+++ main.js
@@ -1,82 +1,73 @@
+let map = ['Euroasia', 'America', 'Greenland', 'Africa', 'Australia'],
+    continentInfo = document.querySelector('.travel-map__list'),
+    travelMapArrow = document.getElementById('travelArrow');
 
 
+function viewTravels(e) {
+    console.log(e.layerY)
+    let clientX = e.layerX,
+        clientY = e.layerY,
+        travelMap = document.getElementById('travel-map');
 
-let euroAsia = document.querySelector('.EuroAsia');
-let america = document.querySelector('.America');
-let greenland = document.querySelector('.Greenland');
-let africa = document.querySelector('.Africa');
-let australia = document.querySelector('.Australia');
-let continentInfo = document.querySelector('.travel-map__list');
 
-euroAsia.addEventListener('click', function() {
-    if (euroAsia.classList.contains('continent--disable')) {
-        euroAsia.classList.remove('continent--disable')
-        euroAsia.classList.add('continent--active')
-        continentInfo.classList.remove('travel-map__list--disable')
-        continentInfo.classList.add('travel-map__list--active')
+    if (clientX > 300) {
+        travelMap.style.left = clientX - 350 + 'px'
+        travelMapArrow.style.left = clientX - 128 + 'px'
+        travelMapArrow.style.transform = 'rotate(317deg)'
     } else {
-        euroAsia.classList.add('continent--disable')
-        euroAsia.classList.remove('continent--active')
-        continentInfo.classList.add('travel-map__list--disable')
-        continentInfo.classList.remove('travel-map__list--active')
+        travelMap.style.left = clientX + 90 + 'px'
+        travelMapArrow.style.left = clientX - 25 + 'px'
+        travelMapArrow.style.transform = 'rotate(230deg)'
     }
-}) 
 
-america.addEventListener('click', function() {
-    if (america.classList.contains('continent--disable')) {
-        america.classList.remove('continent--disable')
-        america.classList.add('continent--active')
-        continentInfo.classList.remove('travel-map__list--disable')
-        continentInfo.classList.add('travel-map__list--active')
+    if (clientY > 240) {
+        travelMap.style.top = 240 + 'px'
+        travelMapArrow.style.left = clientX - 148 + 'px'
+        travelMapArrow.style.transform = 'rotate(343deg)'
+        travelMapArrow.style.top = clientY + 10 + 'px'
+    } else if (clientY > 100){
+        travelMap.style.top = clientY + 'px'
+        travelMapArrow.style.top = clientY + 44  + 'px'
     } else {
-        america.classList.add('continent--disable')
-        america.classList.remove('continent--active')
-        continentInfo.classList.add('travel-map__list--disable')
-        continentInfo.classList.remove('travel-map__list--active')
+        console.log('2 else')
+        travelMap.style.top = clientY + 'px'
+        travelMapArrow.style.top = clientY + 45 + 'px'
     }
-}) 
+}
 
-greenland.addEventListener('click', function() {
-    if (greenland.classList.contains('continent--disable')) {
-        greenland.classList.remove('continent--disable')
-        greenland.classList.add('continent--active')
-        continentInfo.classList.remove('travel-map__list--disable')
-        continentInfo.classList.add('travel-map__list--active')
-    } else {
-        greenland.classList.add('continent--disable')
-        greenland.classList.remove('continent--active')
-        continentInfo.classList.add('travel-map__list--disable')
-        continentInfo.classList.remove('travel-map__list--active')
-    }
-}) 
+map.forEach((cont) => {
+    let currentContinent = document.getElementById(cont);
 
-africa.addEventListener('click', function() {
-    if (africa.classList.contains('continent--disable')) {
-        africa.classList.remove('continent--disable')
-        africa.classList.add('continent--active')
-        continentInfo.classList.remove('travel-map__list--disable')
-        continentInfo.classList.add('travel-map__list--active')
-    } else {
-        africa.classList.add('continent--disable')
-        africa.classList.remove('continent--active')
-        continentInfo.classList.add('travel-map__list--disable')
-        continentInfo.classList.remove('travel-map__list--active')
-    }
-}) 
+    currentContinent.addEventListener('click', (e)=>{
+        document.getElementById('travel-map__description').innerText = cont
+        map.forEach((checkCont) => {
+            if (document.getElementById(checkCont).classList.contains('continent--active')) {
+                document.getElementById(checkCont).classList.remove('continent--active')
+                document.getElementById(checkCont).classList.add('continent--disable')
+            }
+        })
 
-australia.addEventListener('click', function() {
-    if (australia.classList.contains('continent--disable')) {
-        australia.classList.remove('continent--disable')
-        australia.classList.add('continent--active')
-        continentInfo.classList.remove('travel-map__list--disable')
-        continentInfo.classList.add('travel-map__list--active')
-    } else {
-        australia.classList.add('continent--disable')
-        australia.classList.remove('continent--active')
-        continentInfo.classList.add('travel-map__list--disable')
-        continentInfo.classList.remove('travel-map__list--active')
-    }
-}) 
+        if (currentContinent.classList.contains('continent--disable')) {
+            currentContinent.classList.add('continent--active')
+            continentInfo.classList.add('travel-map__list--active')
+            travelMapArrow.classList.add('travel-map__list--active')
+    
+            currentContinent.classList.remove('continent--disable')
+            continentInfo.classList.remove('travel-map__list--disable')
+            travelMapArrow.classList.remove('travel-map__list--disable')
+            viewTravels(e)
+        } else {
+            currentContinent.classList.add('continent--disable')
+            continentInfo.classList.add('travel-map__list--disable')
+            travelMapArrow.classList.add('travel-map__list--disable')
+    
+            currentContinent.classList.remove('continent--active')
+            continentInfo.classList.remove('travel-map__list--active')
+            travelMapArrow.classList.remove('travel-map__list--active')
+        }
+    })
+})
 
 
-baron('#style-1')
+
+/* baron('#style-1') */
